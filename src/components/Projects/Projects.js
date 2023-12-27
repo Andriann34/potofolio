28591ba45e1ccf6cd11ffdc3b516575fa5114838
Andrian43 <117629945+Andrian43@users.noreply.github.com>
2023-12-27 21:51:11 +0700

    Add files via upload

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import IoT from "../../Assets/Projects/IoT.jpeg";
+import Inventory from "../../Assets/Projects/Inventory.png";
 
 function Projects() {
   return (
@@ -20,8 +21,17 @@ function Projects() {
             <ProjectCard
               imgPath={IoT}
               isBlog={false}
-              title="IoT"
-              description="Creating a presence application Using IoT-based RFID, making this application so that it is effective there is no cheating because this is automatic when we bring the card closer to the RFID reader, our name will automatically be uploaded immediately."
+              title="Presence Application Using IoT Based RFID"
+              description="I made this Project 1 in the 3rd semester for approximately 4-5 months, using several hardware and programming languages, for hardware I used NodeMCU ESP8266, Button, LED Lights, Jumper Cables, RFID TAG and RFID Reader, for programming language I used C++, PHP and MySQL as database."
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Inventory}
+              isBlog={false}
+              title="Inventory Management System Using the Laravel Framework"
+              description="This project 2 is in the process of being made from October until now, using the Laravel Framework, PHP, MySQL, and Bootstrap."
             />
           </Col>
         </Row>
